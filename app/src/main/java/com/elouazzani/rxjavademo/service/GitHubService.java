0a82ba64649d998a3da53ebdcc91d3f8c24e2921
User <abdrahamnelouazzani0@gmail.com>
2021-02-19 16:49:04 +0100

    setup service and client class

---
+++ app/src/main/java/com/elouazzani/rxjavademo/service/GitHubService.java
@@ -0,0 +1,12 @@
+package com.elouazzani.rxjavademo.service;
+
+import com.elouazzani.rxjavademo.model.GitHubRepo;
+import java.util.List;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import rx.Observable;
+
+public interface GitHubService {
+    @GET("users/{user}/starred")
+    Observable<List<GitHubRepo>> getStartedRepositories(@Path("user") String name);
+}
